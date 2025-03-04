aecbaca502f57497f67856be2e3dc52f0ce3faf9
tomichal <michalt48@gmail.com>
2025-03-04 08:14:36 -0800

    Update main.R

--- main.R
+++ main.R
@@ -1,18 +1,2 @@
-
 options(repos = c(CRAN = "https://cloud.r-project.org/"))
 getOption("repos")
-
-# R.version
-
-# BiocManager::install("BiocParallel", force = TRUE)
-
-# install.packages("BiocManager")
-# install.packages('BiocParallel')
-# install.packages('rmarkdown')
-# install.packages('knitr')
-# BiocManager::install('zinbwave')
-# install.packages('readr')
-# install.packages('tidyr')
-# install.packages('tibble')
-BiocManager::install("biomaRt")
-# install.packages('dplyr')
