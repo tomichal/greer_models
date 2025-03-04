d7edb4d2f9e8af5df54a642fe3d205f485f1c37d
tomichal <michalt48@gmail.com>
2025-03-03 19:01:32 -0800

    Scratch

---
+++ main.R
@@ -0,0 +1,18 @@
+
+options(repos = c(CRAN = "https://cloud.r-project.org/"))
+getOption("repos")
+
+# R.version
+
+# BiocManager::install("BiocParallel", force = TRUE)
+
+# install.packages("BiocManager")
+# install.packages('BiocParallel')
+# install.packages('rmarkdown')
+# install.packages('knitr')
+# BiocManager::install('zinbwave')
+# install.packages('readr')
+# install.packages('tidyr')
+# install.packages('tibble')
+BiocManager::install("biomaRt")
+# install.packages('dplyr')
