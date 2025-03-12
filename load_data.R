dee1e408183cf5de8ff4ccacb1396eee7c5920bd
tomichal <michalt48@gmail.com>
2025-03-11 20:26:01 -0700

    load pc_genes

--- load_data.R
+++ load_data.R
@@ -1,6 +1,6 @@
 # load_data.R
 
-load_data <- function(counts_filepath, metadata_filepath) {
+load_data <- function(metadata_filepath, counts_filepath, pc_genes_filepath) {
   ms_counts <- read_csv(counts_filepath)
   counts <- ms_counts %>%
     as.data.frame() %>%
@@ -14,13 +14,13 @@ load_data <- function(counts_filepath, metadata_filepath) {
 
   matrix_ms_predrop <- as.matrix(subset_data_ms_drop)
 
-  # pc_genes <-read.delim("~/biomart_protein_coding_genes_withgeneinfo.tsv", stringsAsFactors = FALSE)
+  pc_genes <- read_csv(pc_genes_filepath)
 
   list(
     # counts = counts,
     meta_MS = meta_MS,
-    matrix_ms_predrop = matrix_ms_predrop
-    # pc_genes = pc_genes
+    matrix_ms_predrop = matrix_ms_predrop,
+    pc_genes = pc_genes
   )
 }
 
